3f0be8c390b1825b28c56898b78d206b663a87de
may.hoang@student.rocvf.nl <H0an91123581321>
2021-06-16 05:53:59 +0200

    finished opdracht3_6.php, opdracht3_7.php and opdracht4_1.php

--- H03/opdracht3_6.php
+++ H03/opdracht3_6.php
@@ -13,18 +13,53 @@
 </head>
 <body>
 <?php
-$zwemclub["De spartelkuikens "] = 25;
-$zwemclub["De waterbuffels "] = 32;
-$zwemclub["Plonsmderin "] = 11;
-$zwemclub["Bommetje "] = 23;
+$spartelkuikens = 25;
+$waterbuffels = 32;
+$plonsmderin = 11;
+$bommetje = 23;
 
+$clubs = array($spartelkuikens, $waterbuffels, $plonsmderin, $bommetje);
 
-foreach($zwemclub as $club => $club_leden) {
+function doTheEcho($number) {
+    for($i = 0; $i < $number; $i++) {
+        echo "<img src='../img/zwemmer.png'>";
+    }
+}
 
-        echo "$club $club_leden<br>";
-       if($club_leden % 5 == 5) {
-           $club_leden="img src=../img/zwemmer.png";
-       }
+foreach($clubs as $club) {
+    echo "$club ";
+
+    switch ($club) {
+        case $club < 5:
+            doTheEcho(0);
+            break;
+
+        case $club < 10:
+            doTheEcho(1);
+            break;
+
+        case $club < 15:
+            doTheEcho(2);
+            break;
+
+        case $club < 20:
+            doTheEcho(3);
+            break;
+
+        case $club < 25:
+            doTheEcho(4);
+            break;
+
+        case $club < 30:
+            doTheEcho(5);
+            break;
+
+        case $club < 35:
+            doTheEcho(6);
+            break;
+
+    }
+
 }
 
 ?>
