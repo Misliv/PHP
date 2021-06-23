da2a0e94008c21437e296f365073b527bd0d9c94
may.hoang@student.rocvf.nl <H0an91123581321>
2021-06-23 06:27:47 +0200

    finished H5

--- H05/loginresponse.php
+++ H05/loginresponse.php
@@ -1,1 +1,20 @@
 <?php
+$loggedIn = false;
+if ($_GET != null) {
+    $email = $_GET["email"];
+    $password = $_GET["password"];
+    login($email, $password);
+}
+function login($email, $password) {
+    if ($email == "piet@worldonline.nl" && $password == "doetje123" || $email == "klaas@carpets.nl" && $password == "snoepje777" || $email == "truushendriks@wegweg.nl" && $password == "arkiearkie201") {
+        $loggedIn = true;
+        if ($loggedIn) {
+            echo 'Welkom';
+        }
+    } else {
+        $loggedIn = false;
+        if (!$loggedIn) {
+            echo 'Sorry, geen toegang!';
+        }
+    }
+}
