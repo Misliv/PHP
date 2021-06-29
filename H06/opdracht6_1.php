8a32e37b4175dab5cf81308944dc7eb312dfcaa7
may.hoang@student.rocvf.nl <H0an91123581321>
2021-06-29 22:07:21 +0200

    changed code

--- H06/opdracht6_1.php
+++ H06/opdracht6_1.php
@@ -1,3 +1,16 @@
 <?php
-$dbh = new PDO('mysql:host=localhost;dbname=database;port=3306',
-    'root', '');
+
+
+$user = "root";
+$pass = "root";
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=school; port=3306', $user, $pass);
+    foreach($dbh->query('SELECT * from cursist') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
