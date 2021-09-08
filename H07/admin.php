9cdc3e752143739731f25d840bcde9123a99fa1e
Misliv <may.hoang@student.rocvf.nl>
2021-09-08 09:26:35 +0200

    removed another login.php from admin.php

--- H07/admin.php
+++ H07/admin.php
@@ -3,7 +3,6 @@
 
 if (isset($_SESSION["user"]) && $_SESSION["user"]["rol"] == "Administrator") {
      echo "<h1>Welkom ".$_SESSION["user"]["naam"]. " op het admingedeelte van de website</h1>";
-     echo "<p><a href='login.php'>login</a></p>";
 
  } else if (isset($_SESSION["user"]) && $_SESSION["user"]["rol"] != "Administrator") {
     echo "<h1>Onvoldoende rechten om de pagina te bezoeken</h1>";
