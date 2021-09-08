fd669e48aff966b230ad1a2068449af38fbe749e
Misliv <may.hoang@student.rocvf.nl>
2021-09-08 09:23:12 +0200

    im baaaack. admin.php tells you now if you can access the site or not depending if you're an admin or a user
    removed a few now irrelevant things like redirect to login
    
    removed opdracht7.html

--- H07/admin.php
+++ H07/admin.php
@@ -4,6 +4,13 @@
 if (isset($_SESSION["user"]) && $_SESSION["user"]["rol"] == "Administrator") {
      echo "<h1>Welkom ".$_SESSION["user"]["naam"]. " op het admingedeelte van de website</h1>";
      echo "<p><a href='login.php'>login</a></p>";
- } else {
-     header('location: login.php');
- }
+
+ } else if (isset($_SESSION["user"]) && $_SESSION["user"]["rol"] != "Administrator") {
+    echo "<h1>Onvoldoende rechten om de pagina te bezoeken</h1>";
+
+}
+
+?>
+
+<p><a href="website.php">Terug naar de website</a></p>
+<p><a href="login.php?loguit">Uitloggen</a></p>
--- H07/website.php
+++ H07/website.php
@@ -3,13 +3,11 @@
 
  if(isset($_SESSION["user"])) {
      echo "<h1>Welkom " . $_SESSION["user"]["naam"] . " met de rol " . $_SESSION["user"]["rol"] . " op de website</h1>";
-     echo "<p><a href='login.php'>login</a></p>";
 
 
  } else {
      header('location: login.php');
  }
-
 
  ?>
 
