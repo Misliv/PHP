9305da515b9f0e01206dea442c9bca3c777cdda1
Misliv <may.hoang@student.rocvf.nl>
2021-09-10 15:25:34 +0200

    remaking hoofdstuk 3 as practice

--- practice/H3/1.php
+++ practice/H3/1.php
@@ -1,2 +1,22 @@
 <?php
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP Opdrachten</title>
+</head>
+<body>
+<?php
+for ($i = 1; $i<=9; $i++) {
+    echo "<img src='../img/apen/aap".$i.".jpg'>";
+}
+
+$bomen = array("img_0050.jpg", "lillypilly1.jpg", "Maranchery_Biyyam_Kayal_kandal.jpg", "weeping-elm.jpg", "weeping-elm0091.jpg");
+
+foreach($bomen as $boom) {
+    echo "<img src='../img/bomen/".$boom."'>";
+}
+?>
+</body>
+</html>
