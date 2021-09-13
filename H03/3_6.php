e0a370dda304412331795887bf09aa5a34d50fed
Misliv <may.hoang@student.rocvf.nl>
2021-09-13 14:31:59 +0200

    addded table

--- H03/3_6.php
+++ H03/3_6.php
@@ -1,1 +1,36 @@
 <?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Zwemclubs</title>
+    <style>
+        table, td {
+            outline: black solid 1px;
+            border-collapse: collapse;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+<table>
+    <tr>
+        <td>De spartelkuikens</td>
+        <td>25</td>
+    </tr>
+    <tr>
+        <td>De waterbuffels</td>
+        <td>32</td>
+    </tr>
+    <tr>
+        <td>Plonsmderin</td>
+        <td>11</td>
+    </tr>
+    <tr>
+        <td>Bommetje</td>
+        <td>23</td>
+    </tr>
+</table>
+</body>
+</html>
