40d6ec0ae1ca2b713c0768aecc194513d34c8ce9
Misliv <may.hoang@student.rocvf.nl>
2021-09-13 09:44:18 +0200

    opdracht 3.1 klaar, laat apen en bomen zien, index updated

---
+++ H03/3_1.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Opdrachten</title>
+</head>
+<body>
+<?php
+    for ($i = 1; $i<=9; $i++) {
+        echo "<img src='../img/apen/aap".$i.".jpg'>";
+    }
+    ?>
+</body>
+</html>
