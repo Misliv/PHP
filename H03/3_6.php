58afe8d30f253de41647cfc30a3a09a8541be495
Misliv <may.hoang@student.rocvf.nl>
2021-09-14 12:52:54 +0200

    3.6, i guess it works? but its not complete

--- H03/3_6.php
+++ H03/3_6.php
@@ -11,10 +11,52 @@
 </head>
 <body>
 <?php
-$zwemmers = array("De spartelkuikens" => 25, "De waterbuffels" => 32, "Plonsmderin" => 11, "Bommetje" => 23);
+$spartelkuikens = 25;
+$waterbuffels = 32;
+$plonsmderin = 11;
+$bommetje = 23;
 
-foreach ($zwemmers as $zwem => $aantal) {
-    echo " ".$zwem." ".$aantal."<br>";
+$zwemmers = array($spartelkuikens, $waterbuffels, $plonsmderin, $bommetje);
+
+function DeEcho($number) {
+    for ($i = 0; $i < $number; $i++) {
+        echo "<img src='../img/zwemmer.png'>";
+    }
+}
+
+foreach ($zwemmers as $zwem) {
+    echo "".$zwem."";
+
+switch ($zwem) {
+    case $zwem < 5:
+        DeEcho(0);
+        break;
+
+        case $zwem < 10:
+        DeEcho(1);
+        break;
+
+        case $zwem < 15:
+        DeEcho(2);
+        break;
+
+        case $zwem < 20:
+        DeEcho(3);
+        break;
+
+        case $zwem < 25:
+        DeEcho(4);
+        break;
+
+        case $zwem < 30:
+        DeEcho(5);
+        break;
+
+        case $zwem < 35:
+        DeEcho(6);
+        break;
+
+}
 }
 ?>
 </body>
