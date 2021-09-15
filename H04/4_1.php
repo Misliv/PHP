f5f66667e79ecd8afabca3478726f9be7f7e8b01
Misliv <may.hoang@student.rocvf.nl>
2021-09-15 10:50:30 +0200

    4_1 done, converts celsius to fahrenheit

--- H04/4_1.php
+++ H04/4_1.php
@@ -1,1 +1,23 @@
 <?php
+    if ($_POST) {
+        $c = $_POST['celsius'];
+        $f = $c * 1.8 + 32;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Celsius naar Fahrenheit</h1>
+
+<form method="post">
+    <input type="text" name="celsius"><br>
+    <?php
+    if (isset($f)){
+        echo "fahrenheit = ".$f;
+    }
+    ?>
+</form>
+</body>
+</html>
