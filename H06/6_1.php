3bd299a4ab169a10de19e64ef26413762e83d304
Misliv <may.hoang@student.rocvf.nl>
2021-09-20 13:53:25 +0200

    6_1 done

--- H06/6_1.php
+++ H06/6_1.php
@@ -1,1 +1,12 @@
 <?php
+try {
+$dbh = new PDO('mysql:host=localhost;dbname=phpschool;port=3306', 'root', '');
+foreach ($dbh->query('SELECT * FROM cursist') as $row) {
+    print_r($row);
+}
+$dbh = null;
+} catch (PDOException $e) {
+    print "Error!: ".$e->getMessage()."<br/>";
+    die();
+}
+?>
