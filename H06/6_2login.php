ec37ef5dd33808554a9a935d7d6bbb6f8aeefd1a
Misliv <may.hoang@student.rocvf.nl>
2021-09-21 11:29:14 +0200

    fixed missing {

--- H06/6_2login.php
+++ H06/6_2login.php
@@ -6,52 +6,52 @@
         $email_err = $wachtwoord_err = $login_err = "";
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (empty(trim($_POST["email    "]))){
+            if (empty(trim($_POST["email    "]))) {
                 $email_err = "Zet een email in.";
             } else {
                 $email = trim($_POST['email']);
             }
-        if (empty(trim($_POST["wachtwoord"]))) {
-            $wachtwoord_err = "Zet een wachtwoord in.";
-        } else {
-            $wachtwoord = trim($_POST['wachtwoord']);
-        }
-    }
+            if (empty(trim($_POST["wachtwoord"]))) {
+                $wachtwoord_err = "Zet een wachtwoord in.";
+            } else {
+                $wachtwoord = trim($_POST['wachtwoord']);
+            }
 
-        if (empty($email_err) && empty($wachtwoord_err)) {
-            $sql = "SELECT email, wachtwoord FROM login WHERE email = ?";
-            if ($stmt = mysqli_prepare($link, $sql)) {
-                mysqli_stmt_bind_param($stmt, "s", $param_email);
+            if (empty($email_err) && empty($wachtwoord_err)) {
+                $sql = "SELECT email, wachtwoord FROM login WHERE email = ?";
+                if ($stmt = mysqli_prepare($link, $sql)) {
+                    mysqli_stmt_bind_param($stmt, "s", $param_email);
 
-                $param_email = $email;
+                    $param_email = $email;
 
-                if (mysqli_stmt_execute($stmt)) {
-                    mysqli_stmt_store_result($stmt);
+                    if (mysqli_stmt_execute($stmt)) {
+                        mysqli_stmt_store_result($stmt);
 
-                    if (mysqli_stmt_num_rows($stmt) == 1) {
-                        mysqli_stmt_bind_result($stmt, $username, $hashed_wachtwoord);
-                        if(mysqli_stmt_fetch($stmt)) {
-                            if (password_verify($wachtwoord, $hashed_wachtwoord)) {
-                                session_start();
+                        if (mysqli_stmt_num_rows($stmt) == 1) {
+                            mysqli_stmt_bind_result($stmt, $username, $hashed_wachtwoord);
+                            if (mysqli_stmt_fetch($stmt)) {
+                                if (password_verify($wachtwoord, $hashed_wachtwoord)) {
+                                    session_start();
 
-                                $_SESSION["loggedin"] = true;
-                                $_SESSION["email"] = $email;
+                                    $_SESSION["loggedin"] = true;
+                                    $_SESSION["email"] = $email;
 
-                                header("location: welkom.php");
-                            } else {
-                                $login_err = "Email en/of wachtwoord kloppen niet.";
+                                    header("location: welkom.php");
+                                } else {
+                                    $login_err = "Email en/of wachtwoord kloppen niet.";
+                                }
                             }
+                        } else {
+                            $login_err = "Email en/of wachtwoord kloppen niet.";
                         }
                     } else {
-                        $login_err = "Email en/of wachtwoord kloppen niet.";
+                        echo "Oh nee! Iets ging verkeerd. Probeer nog een keer later.";
                     }
-                } else {
-                    echo "Oh nee! Iets ging verkeerd. Probeer nog een keer later.";
+                    mysqli_stmt_close($stmt);
                 }
-                mysqli_stmt_close($stmt);
             }
+            mysqli_close($link);
         }
-        mysqli_close($link);
 ?>
 
 <!DOCTYPE html>
