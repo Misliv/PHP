86ee1eeb343892ef8a268e09e646350a01848e75
Misliv <may.hoang@student.rocvf.nl>
2021-09-29 09:19:03 +0200

    removed 7v4_2

--- H7/H7v4_2/7v4adminpage.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<head>
-    <title>Adminpage</title>
-</head>
-<body>
-<h1>Hallo admin!</h1>
-<p><a href="7v4userpage.php">Terug naar de website</a></p>
-<p><a href="7v4login.php?loguit">Uitloggen</a></p>
-</body>
