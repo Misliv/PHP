1f8da576f2f1a9bf6e57952e06f4e1beb67c8960
Misliv <may.hoang@student.rocvf.nl>
2021-09-30 13:24:28 +0200

    fixed headers

--- H7 other versions/H7v3/7v3welkom.php
+++ H7 other versions/H7v3/7v3welkom.php
@@ -1,13 +1,13 @@
 <?php
-include "6_2config.php";
+include "7v3config.php";
 
 if(!isset($_SESSION['email'])){
-    header('Location: 6_2login.php');
+    header('Location: 7v3login.php');
 }
 
 if(isset($_POST['but_logout'])){
     session_destroy();
-    header('Location: 6_2login.php');
+    header('Location: 7v3login.php');
 }
 ?>
 <!doctype html>
