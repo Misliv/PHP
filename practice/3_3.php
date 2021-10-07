824f7638969621798761ca79983804a4cea454a1
Misliv <may.hoang@student.rocvf.nl>
2021-10-07 09:41:13 +0200

    3_3 done

--- practice/3_3.php
+++ practice/3_3.php
@@ -1,10 +1,9 @@
 <?php
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>PHP Opdrachten</title>
+    <title>christmas borders</title>
     <style>
         body {
             text-align: center;
@@ -21,14 +20,15 @@
 </head>
 <body>
 <?php
-    for ($i = 1; $i<=9; $i++) {
+    for ($i = 1; $i <= 9; $i++) {
         if ($i % 2 == 0) {
             $class = "class='rood'";
         } else {
             $class = "class='groen'";
         }
-        echo "<img ".$class. "src='../img/apen/aap" . $i . ".jpg'>";
+        echo "<img ".$class."src='../img/apen/aap".$i.".jpg'>";
     }
-    ?>
+?>
 </body>
 </html>
+
